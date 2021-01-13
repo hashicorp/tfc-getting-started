4d4848e243a037c0f46fef0cdec70e2a6b6d77e6
Chris Arcand <chris@chrisarcand.com>
2021-01-12 22:51:02 -0600

    Add FWS provider configuration

--- main.tf
+++ main.tf
@@ -10,13 +10,48 @@ terraform {
   }
 }
 
-resource "random_pet" "animal" {
-  keepers = {
-    uuid = uuid() # Force a new name each time
-  }
-  length = 3
+# The following configuration uses a provider which provisions [fake] resources
+# to a ficiticious cloud vendor called "Fake Web Services".
+#
+# After running the setup script (./scripts/setup.sh), feel free to change these
+# resources and 'terraform apply' as much as you'd like! These resources are
+# purely for demonstration and created in Terraform Cloud, scoped to your TFC
+# user account.
+#
+# To review the provider and documentation for the available resources and
+# schemas, see: https://registry.terraform.io/providers/hashicorp/fakewebservices
+
+# The following variable is used to configure the provider's authentication
+# token You don't need to provide a token on the command line to apply changes,
+# though: using the remote backend, Terraform will execute remotely in Terraform
+# Cloud where your token is already securely stored in your workspace!
+variable "provider_token" {
+  type = string
 }
 
-output "random" {
-  value = random_pet.animal.id
+provider "fakewebservices" {
+  token = var.provider_token
+}
+
+resource "fakewebservices_vpc" "primary_vpc" {
+  name = "Primary VPC"
+  cidr_block = "0.0.0.0/1"
+}
+
+resource "fakewebservices_server" "servers" {
+  count = 2
+
+  name = "Server ${count.index+1}"
+  type = "t2.micro"
+  vpc = fakewebservices_vpc.primary_vpc.name
+}
+
+resource "fakewebservices_load_balancer" "primary_lb" {
+  name = "Primary Load Balancer"
+  servers = fakewebservices_server.servers[*].name
+}
+
+resource "fakewebservices_database" "prod_db" {
+  name = "Production DB"
+  size = 256
 }
