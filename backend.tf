c194556435dd9c928110496ff37c68bd6a008525
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2021-08-02 14:59:42 +0900

    add server

--- backend.tf
+++ backend.tf
@@ -2,10 +2,10 @@
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
   backend "remote" {
-    organization = "{{ORGANIZATION_NAME}}"
+    organization = "example-org-c5ad07"
 
     workspaces {
-      name = "{{WORKSPACE_NAME}}"
+      name = "getting-started"
     }
   }
 
