f0e35031bf623aaffea88ab7d8b8d935a95a92c4
waynerferreira <waynerferreira@hotmail.com>
2021-08-30 10:23:00 -0300

    up up

---
+++ backend.tf
@@ -0,0 +1,13 @@
+
+/*terraform {
+  backend "remote" {
+    organization = "{{ORGANIZATION_NAME}}"
+
+    workspaces {
+      name = "{{WORKSPACE_NAME}}"
+    }
+  }
+
+  required_version = ">= 0.13.0"
+}
+*/
