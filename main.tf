2f55303394fe4ed11fe0e08d55cde61a7c7866d1
waynerferreira <waynerferreira@hotmail.com>
2021-08-30 10:22:06 -0300

    up arquivos tf

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-    region = "us-east-1"
-    version = "~> 3.0"
-  }
-terraform {
+ terraform {
     backend "s3" {
       bucket = "waynerferreiraaws2"
     #  dynamodb_table = "terraform-state-lock-dynamo"
@@ -29,5 +25,29 @@ resource "aws_instance" "srvk8s" {
     subnet_id = "subnet-0066472a5254ee7ef"
     vpc_security_group_ids = ["${aws_security_group.sg_teste.id}"]
     key_name = var.key_name
+}
+*/
+
+/*resource "fakewebservices_vpc" "primary_vpc" {
+  name       = "Primary VPC"
+  cidr_block = "0.0.0.0/1"
+}
+
+resource "fakewebservices_server" "servers" {
+  count = 2
+
+  name = "Server ${count.index + 1}"
+  type = "t2.micro"
+  vpc  = fakewebservices_vpc.primary_vpc.name
+}
+
+resource "fakewebservices_load_balancer" "primary_lb" {
+  name    = "Primary Load Balancer"
+  servers = fakewebservices_server.servers[*].name
+}
+
+resource "fakewebservices_database" "prod_db" {
+  name = "Production DB"
+  size = 256
 }
 */
