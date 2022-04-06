6eabe1d3385ecb5a227efed52c1e5a3fa7c9ff2c
hashimoon <98980386+hashimoon@users.noreply.github.com>
2022-04-06 15:30:48 -0700

    Remove provider code

--- provider.tf
+++ provider.tf
@@ -3,11 +3,3 @@
 # though: using the remote backend, Terraform will execute remotely in Terraform
 # Cloud where your token is already securely stored in your workspace!
 
-variable "provider_token" {
-  type = string
-  sensitive = true
-}
-
-provider "fakewebservices" {
-  token = var.provider_token
-}
