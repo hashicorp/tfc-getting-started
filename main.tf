a2bb9829f13c08c16e0ab677c682004d24f7d77a
Salil Subbakrishna <salil.subbakrishna@hashicorp.com>
2022-08-12 17:48:17 -0700

    Adding an extra server

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "fakewebservices_vpc" "primary_vpc" {
 }
 
 resource "fakewebservices_server" "servers" {
-  count = 2
+  count = 3
 
   name = "Server ${count.index + 1}"
   type = "t2.micro"
